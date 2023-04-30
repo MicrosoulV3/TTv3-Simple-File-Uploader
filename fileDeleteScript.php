40f14cd773e4873eb98f9741f7b0310709128144
MicrosoulV3 <37206645+MicrosoulV3@users.noreply.github.com>
2023-04-30 15:00:12 -0400

    realpath update

--- fileDeleteScript.php
+++ fileDeleteScript.php
@@ -10,13 +10,13 @@ begin_frame("delete");
 
 if (isset($_POST['delete']) && isset($_POST['fileToDelete'])) {
     $fileToDelete = $_POST['fileToDelete'];
-    $filePath = $_SERVER['DOCUMENT_ROOT'] . $uploadDirectory . $fileToDelete;
+    $filePath = realpath($_SERVER['DOCUMENT_ROOT']) . $uploadDirectory . $fileToDelete;
 
     if (file_exists($filePath)) {
         unlink($filePath);
-        autolink ("uploader.php", "<b><font color='#ff0000'>File Deleted....</font></b>");
+        autolink("uploader.php", "<b><font color='#ff0000'>File Deleted....</font></b>");
     } else {
-        autolink ("uploader.php", "<b><font color='#ff0000'>File Not Found....</font></b>");
+        autolink("uploader.php", "<b><font color='#ff0000'>File Not Found....</font></b>");
     }
 }
 
