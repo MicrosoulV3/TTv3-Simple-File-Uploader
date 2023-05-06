950da8082fbb7080032535eaf8ee669525814f81
MicrosoulV3 <37206645+MicrosoulV3@users.noreply.github.com>
2023-05-06 00:21:00 -0400

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -1,4 +1,5 @@
-<!-- Simple file uploader for TTv3 Apr 15 2023 MicroMonkey-->
+<!-- Simple file uploader for TTv3 may 5 2023 MicroMonkey-->
+<!-- The 2 other files are fileUploadScript and fileDeleteScript -->
 <?php
 require_once("backend/functions.php");
 dbconn(false);
@@ -13,21 +14,29 @@ $maxFileSizeMB = $maxFileSize / (1024 * 1024); // Convert bytes to megabytes. DO
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
 <meta charset="UTF-8" http-equiv="refresh" content="100" border="none">
    <link rel="stylesheet" type="text/css" href="<?php echo $site_config['SITEURL'];?>/themes/<?php echo $THEME;?>/themes.css" />
     <title>PHP File Upload</title>
     <script>
+
 function validateFile() {
     const fileInput = document.getElementById("fileToUpload");
-    
+
     if (!fileInput.files || !fileInput.files[0]) {
         alert("Please select a file to upload.");
         return false;
     }
-    
+    const fileName = fileInput.files[0].name;
     const fileSize = fileInput.files[0].size; // file size in bytes
     const maxFileSizeMB = <?php echo $maxFileSizeMB; ?>;
     const maxFileSizeBytes = maxFileSizeMB * 1024 * 1024; // convert to bytes
+    const fileExtensionsAllowed = <?php echo json_encode($fileExtensionsAllowed); ?>; // allowed file extensions. Defined in config.php
+    const fileExtension = fileName.split('.').pop().toLowerCase();
+    if (!fileExtensionsAllowed.includes(fileExtension)) {
+        alert("Invalid file type. You are trying to upload a ." + fileExtension + " file. Allowed file types are: " + fileExtensionsAllowed.join(', '));
+        return false;
+    }
 
     if (fileSize > maxFileSizeBytes) {
         const fileSizeMB = Math.round(fileSize / (1024 * 1024));
@@ -38,30 +47,39 @@ function validateFile() {
 }
     </script>
 </head>
+
 <body>
     <form action="fileUploadScript.php" method="post" enctype="multipart/form-data" onsubmit="return validateFile()">
         Upload a File:
         <input type="file" name="the_file" id="fileToUpload">
         <input type="submit" name="submit" value="Send it">
-        <p>Maximum file size: <span style='color: green;'><?php echo $maxFileSizeMB; ?></span> MB</p>
-        <p>Allowed Types: <span style='color: green;'><?php echo implode(', ', $fileExtensionsAllowed); ?></span></p>
+        <p>Maximum file size: <span style='color: green; font-weight:bold; font-size:10pt'><?php echo $maxFileSizeMB; ?> MB</span></p>
+        <p>Allowed Types: <span style='color: green; font-weight:bold; font-size:10pt'><?php echo implode(', ', $fileExtensionsAllowed); ?></span></p>
     </form>
 </body>
 </html>
 
 <?php
 function formatBytes($bytes, $decimals = 2) {
-  $size = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'); //completely ridiculous, but meh, why not I guess
+  $size = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
   $factor = floor((strlen($bytes) - 1) / 3);
   return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . ' ' . @$size[$factor];
 }
 
+// Scan the upload directory for files
 $files = scandir($_SERVER['DOCUMENT_ROOT'] . $uploadDirectory);
-$files = array_diff($files, array('.', '..'));
-echo "<br><br>";
-?>
 
+// Remove "." and ".." from the list of files
+$files = array_diff($files, array('.', '..'));
+
+//A few breaks to shift the tables down a little bit
+echo "<br><br>";
+
+// Display a list of all files found
+?>
 <div class="table-container">
+    <h2 style="color: red; text-align: center; display: inline-block;">By using this uploader, you acknowledge that you are using it. Your username will be added onto the front of your uploaded file.</h2>
+        <br><br>
     <h2 style="color: green; display: inline-block;">Uploaded Files:</h2>
 
     <?php if (count($files) > 0): ?>
@@ -153,4 +171,3 @@ echo "<br><br>";
 end_frame();
 stdfoot();
 ?>
-
